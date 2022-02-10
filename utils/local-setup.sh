5d2ab7de59677f37b0c915729004c1719cb19602
craig <cbrookes@redhat.com>
2022-02-10 11:19:08 +0000

    change start up flags to remove no longer used one

--- utils/local-setup.sh
+++ utils/local-setup.sh
@@ -111,7 +111,7 @@ kubectl annotate ingressclass nginx "ingressclass.kubernetes.io/is-default-class
 } &>/dev/null
 
 echo "Starting KCP, sending logs to ${KCP_LOG_FILE}"
-${KCP_BIN} start --push-mode --install-cluster-controller --resources-to-sync=deployments --resources-to-sync=services --resources-to-sync=ingresses.networking.k8s.io --auto-publish-apis > ${KCP_LOG_FILE} 2>&1 &
+${KCP_BIN} start --push-mode  --run-controllers --resources-to-sync=deployments --resources-to-sync=services --resources-to-sync=ingresses.networking.k8s.io --auto-publish-apis > ${KCP_LOG_FILE} 2>&1 &
 KCP_PID=$!
 
 echo "Waiting 15 seconds..."
