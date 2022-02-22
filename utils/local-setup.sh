4e5b0f9408797f9a1d4b16f2a43fdd6011251569
Phil Brookes <pbrookes@redhat.com>
2022-02-22 11:23:35 +0000

    support cluster_num argument

--- utils/local-setup.sh
+++ utils/local-setup.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+usage() { echo "usage: ./local-setup.sh -c <number of clusters>" 1>&2; exit 1; }
+while getopts ":c:" arg; do
+  case "${arg}" in
+    c)
+      NUM_CLUSTERS=${OPTARG}
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+
+if [ -z "${NUM_CLUSTERS}" ]; then
+    usage
+fi
 #
 # Copyright 2021 Red Hat, Inc.
 #
@@ -31,24 +48,19 @@ export KCP_BIN="./bin/kcp"
 TEMP_DIR="./tmp"
 KCP_LOG_FILE="${TEMP_DIR}"/kcp.log
 
-# TODO(jmprusi): Hardcoded, improve.
-KIND_CLUSTER_A="kcp-cluster-a"
-KIND_CLUSTER_B="kcp-cluster-b"
+KIND_CLUSTER_PREFIX="kcp-cluster-"
+for ((i=1;i<=$NUM_CLUSTERS;i++))
+do
+	CLUSTERS="${CLUSTERS}${KIND_CLUSTER_PREFIX}${i} "
+done
 
 mkdir -p ${TEMP_DIR}
 
-
-# TODO(jmprusi): Split this setup into up/clean actions.
-echo "Deleting any previous kind clusters."
-{
-  ${KIND_BIN} delete cluster --name ${KIND_CLUSTER_A}
-  ${KIND_BIN} delete cluster --name ${KIND_CLUSTER_B}
-} &> /dev/null
-
-echo "Deploying two kind k8s clusters locally."
-
-
-cat <<EOF | ${KIND_BIN} create cluster --name ${KIND_CLUSTER_A} --config=-
+createCluster() {
+  cluster=$1;
+  port80=$2;
+  port443=$3;
+  cat <<EOF | ${KIND_BIN} create cluster --name ${cluster} --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
@@ -61,53 +73,44 @@ nodes:
         node-labels: "ingress-ready=true"
   extraPortMappings:
   - containerPort: 80
-    hostPort: 8080
+    hostPort: ${port80}
     protocol: TCP
   - containerPort: 443
-    hostPort: 8443
+    hostPort: ${port443}
     protocol: TCP
 EOF
 
-cat <<EOF | ${KIND_BIN} create cluster --name ${KIND_CLUSTER_B} --config=-
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-nodes:
-- role: control-plane
-  kubeadmConfigPatches:
-  - |
-    kind: InitConfiguration
-    nodeRegistration:
-      kubeletExtraArgs:
-        node-labels: "ingress-ready=true"
-  extraPortMappings:
-  - containerPort: 80
-    hostPort: 8081
-    protocol: TCP
-  - containerPort: 443
-    hostPort: 8444
-    protocol: TCP
-EOF
+  echo "Creating Cluster objects for the kind cluster."
+  ${KIND_BIN} get kubeconfig --name=${cluster} | sed -e 's/^/    /' | cat utils/kcp-contrib/cluster.yaml - | sed -e "s/name: local/name: ${cluster}/" > ${TEMP_DIR}/${cluster}.yaml
 
+  echo "Deploying Ingress controller to kind cluster"
+  {
+  kubectl config use-context kind-${cluster}
 
-echo "Creating Cluster objects for each of the k8s cluster."
+  VERSION=$(curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/stable.txt)
+  curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/"${VERSION}"/deploy/static/provider/kind/deploy.yaml | sed "s/--publish-status-address=localhost/--report-node-internal-ip-address/g" | kubectl apply -f -
+  kubectl annotate ingressclass nginx "ingressclass.kubernetes.io/is-default-class=true"
 
-${KIND_BIN} get kubeconfig --name=${KIND_CLUSTER_A} | sed -e 's/^/    /' | cat utils/kcp-contrib/cluster.yaml - | sed -e "s/name: local/name: ${KIND_CLUSTER_A}/" > ${TEMP_DIR}/${KIND_CLUSTER_A}.yaml
-${KIND_BIN} get kubeconfig --name=${KIND_CLUSTER_B} | sed -e 's/^/    /' | cat utils/kcp-contrib/cluster.yaml - | sed -e "s/name: local/name: ${KIND_CLUSTER_B}/" > ${TEMP_DIR}/${KIND_CLUSTER_B}.yaml
+  } &>/dev/null
+}
 
-echo "Deploying Ingress controller to kind k8s clusters"
+clusterCount=$(${KIND_BIN} get clusters | grep ${KIND_CLUSTER_PREFIX} | wc -l)
+if ! [[ $clusterCount =~ "0" ]] ; then
+  echo "Deleting previous kind clusters."
+  ${KIND_BIN} get clusters | grep ${KIND_CLUSTER_PREFIX} | xargs ${KIND_BIN} delete clusters
+fi
 
-{
-kubectl config use-context kind-${KIND_CLUSTER_A}
+echo "Deploying $NUM_CLUSTERS kind k8s clusters locally."
 
-VERSION=$(curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/stable.txt)
-curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/"${VERSION}"/deploy/static/provider/kind/deploy.yaml | sed "s/--publish-status-address=localhost/--report-node-internal-ip-address/g" | kubectl apply -f -
-kubectl annotate ingressclass nginx "ingressclass.kubernetes.io/is-default-class=true"
-
-kubectl config use-context kind-${KIND_CLUSTER_B}
-curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/"${VERSION}"/deploy/static/provider/kind/deploy.yaml | sed "s/--publish-status-address=localhost/--report-node-internal-ip-address/g" | kubectl apply -f -
-kubectl annotate ingressclass nginx "ingressclass.kubernetes.io/is-default-class=true"
-
-} &>/dev/null
+port80=8080
+port443=8443
+for cluster in $CLUSTERS
+do
+  createCluster "$cluster" $port80 $port443
+  port80=$((port80+1))
+  port443=$((port443+1))
+#move to next cluster
+done
 
 echo "Starting KCP, sending logs to ${KCP_LOG_FILE}"
 ${KCP_BIN} start --push-mode --run-controllers --resources-to-sync=deployments --resources-to-sync=services --resources-to-sync=ingresses.networking.k8s.io --auto-publish-apis > ${KCP_LOG_FILE} 2>&1 &
@@ -141,7 +144,7 @@ echo "example: "
 echo ""
 echo "       ./bin/ingress-controller -kubeconfig .kcp/admin.kubeconfig"
 echo ""
-echo "Dont't forget to export the proper KUBECONFIG to create objects against KCP:"
+echo "Dont forget to export the proper KUBECONFIG to create objects against KCP:"
 echo "export KUBECONFIG=${PWD}/.kcp/admin.kubeconfig"
 echo ""
 read -p "Press enter to exit -> It will kill the KCP process running in background"
